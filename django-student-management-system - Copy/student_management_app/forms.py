f5e98bb1751ea78af5b30b31ac7b279839093803
sleepy <132650214+krugstergaming@users.noreply.github.com>
2024-08-17 18:51:26 +0800

    ok
    ok

---
+++ django-student-management-system - Copy/student_management_app/forms.py
@@ -0,0 +1,113 @@
+from django import forms 
+from django.forms import Form
+from student_management_app.models import YearLevel, SessionYearModel
+
+
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+class AddStudentForm(forms.Form):
+    email = forms.EmailField(label="Email", max_length=50, widget=forms.EmailInput(attrs={"class":"form-control"}))
+    password = forms.CharField(label="Password", max_length=50, widget=forms.PasswordInput(attrs={"class":"form-control"}))
+    first_name = forms.CharField(label="First Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    last_name = forms.CharField(label="Last Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    nickname = forms.CharField(label="Nickname", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    nationality = forms.CharField(label="Nationality", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    religion = forms.CharField(label="Religion", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    # father = forms.CharField(label="Father", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    # mother = forms.CharField(label="Mother", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    
+    #For Displaying yearlevels
+    try:
+        yearlevel = YearLevel.objects.all()
+        yearlevel_list = []
+        for yearlevel in yearlevel:
+            single_yearlevel = (yearlevel.id, yearlevel.yearlevel_name)
+            yearlevel_list.append(single_yearlevel)
+    except:
+        yearlevel_list = []
+    
+    #For Displaying Session Years
+    try:
+        session_years = SessionYearModel.objects.all()
+        session_year_list = []
+        for session_year in session_years:
+            single_session_year = (session_year.id, str(session_year.session_start_year)+" to "+str(session_year.session_end_year))
+            session_year_list.append(single_session_year)
+            
+    except:
+        session_year_list = []
+    
+    gender_list = (
+        ('Male','Male'),
+        ('Female','Female')
+    )
+
+    enrollment_status = (
+    (True, 'Enrolled'),
+    (False, 'Not Enrolled')
+    )
+
+    # family_rank = (
+    # ('1st', '1st'),
+    # ('2nd', '2nd'),
+    # ('3rd', '3rd'),
+    # ('4th', '4th')
+    # )
+
+    
+    yearlevel_id = forms.ChoiceField(label="Yearlevel", choices=yearlevel_list, widget=forms.Select(attrs={"class":"form-control"}))
+    gender = forms.ChoiceField(label="Gender", choices=gender_list, widget=forms.Select(attrs={"class":"form-control"}))
+    session_year_id = forms.ChoiceField(label="Session Year", choices=session_year_list, widget=forms.Select(attrs={"class":"form-control"}))
+    is_enrolled = forms.ChoiceField(label="Enrollment Status", choices=enrollment_status, widget=forms.Select(attrs={"class":"form-control"}))
+    # family_rank = forms.ChoiceField(label="Family Rank", choices=family_rank, widget=forms.Select(attrs={"class":"form-control"}))
+
+    # session_start_year = forms.DateField(label="Session Start", widget=DateInput(attrs={"class":"form-control"}))
+    # session_end_year = forms.DateField(label="Session End", widget=DateInput(attrs={"class":"form-control"}))
+    profile_pic = forms.FileField(label="Profile Pic", required=False, widget=forms.FileInput(attrs={"class":"form-control"}))
+
+
+
+class EditStudentForm(forms.Form):
+    email = forms.EmailField(label="Email", max_length=50, widget=forms.EmailInput(attrs={"class":"form-control"}))
+    first_name = forms.CharField(label="First Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    last_name = forms.CharField(label="Last Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+
+    #For Displaying yearlevels
+    try:
+        yearlevels = YearLevel.objects.all()
+        yearlevel_list = []
+        for yearlevel in yearlevels:
+            single_yearlevel = (yearlevel.id, yearlevel.yearlevel_name)
+            yearlevel_list.append(single_yearlevel)
+    except:
+        yearlevel_list = []
+
+    #For Displaying Session Years
+    try:
+        session_years = SessionYearModel.objects.all()
+        session_year_list = []
+        for session_year in session_years:
+            single_session_year = (session_year.id, str(session_year.session_start_year)+" to "+str(session_year.session_end_year))
+            session_year_list.append(single_session_year)
+            
+    except:
+        session_year_list = []
+
+    
+    gender_list = (
+        ('Male','Male'),
+        ('Female','Female')
+    )
+    
+    yearlevel_id = forms.ChoiceField(label="yearlevel", choices=yearlevel_list, widget=forms.Select(attrs={"class":"form-control"}))
+    gender = forms.ChoiceField(label="Gender", choices=gender_list, widget=forms.Select(attrs={"class":"form-control"}))
+    session_year_id = forms.ChoiceField(label="Session Year", choices=session_year_list, widget=forms.Select(attrs={"class":"form-control"}))
+    # session_start_year = forms.DateField(label="Session Start", widget=DateInput(attrs={"class":"form-control"}))
+    # session_end_year = forms.DateField(label="Session End", widget=DateInput(attrs={"class":"form-control"}))
+    profile_pic = forms.FileField(label="Profile Pic", required=False, widget=forms.FileInput(attrs={"class":"form-control"}))
